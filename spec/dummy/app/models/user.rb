1d1d8d5452fd60233dde17194db0b7c5b98248f6
kaspernj <k@spernj.org>
2024-02-05 05:26:25 +0000

    Added missing ransackable_associations method

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   validates :email, presence: true
 
+  def self.ransackable_associations(_auth_object = nil)
+    []
+  end
+
   def self.ransackable_attributes(_auth_object = nil)
     %i[email encrypted_password]
   end
